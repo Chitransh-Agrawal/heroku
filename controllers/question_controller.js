cb44b02a148e6dd9c86795fdb35a6fd62bab7e9e
Chitransh Agrawal <chitranshagrawal0@gmail.com>
2022-09-30 16:59:54 +0530

    option url dynamically updated

--- controllers/question_controller.js
+++ controllers/question_controller.js
@@ -72,7 +72,7 @@ module.exports.addOptions = function (req, res) {
           question: req.params.id,
           text: req.body.text,
           votes: 0,
-          link: `http://localhost:8000/options/${id}/add_vote`,
+          link: `https://fathomless-springs-22473.herokuapp.com/options/${req.params.id}/add_vote`,
         },
         function (err, optionCreated) {
           if (err) {
